f5f7f4aabfd0251790d0aa678cd4305b9076519e
Daniel <d0583258844@gmail.com>
2025-12-31 13:30:19 +0200

    Added the files to connect the data mongo & sql

---
+++ utils/mongodb.js
@@ -0,0 +1,28 @@
+import MongoClient from "mongodb";
+
+const MONGO_URL =
+  "mongodb://admin:password123@localhost:27018/ecommerce?authSource=admin";
+const Database = "ecommerce";
+const Collection = "products";
+
+let mongoConn = null
+let mongocClient = null
+
+export async function initMongoDb() {
+  try {
+    mongocClient = new MongoClient(MONGO_URL);
+    await mongocClient.connect();
+    mongoConn = mongocClient.db(Database);
+
+    const todosCollection = mongoConn.collection(Collection);
+
+    await todosCollection.createIndex({ name: 1 }, { unique: true });
+
+    console.log(
+      "Database initialized and unique index created on 'name' field"
+    );
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    throw error;
+  }
+}
